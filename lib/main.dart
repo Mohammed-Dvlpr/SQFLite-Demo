bb15fa4d5f2b0fdd3f343c7b609c05452d9ee998
Mohammed Hussain <mohammed.dvlpr@gmail.com>
2024-08-27 14:32:16 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:sqfl_demo_porject/screens/note_list.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NoetKeeper App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        useMaterial3: true,
+      ),
+      home: const NoteList(),
+    );
+  }
+}
---
+++ lib/screens/note_list.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class NoteList extends StatefulWidget {
+  const NoteList({super.key});
+
+  @override
+  State<NoteList> createState() => _NoteListState();
+}
+
+class _NoteListState extends State<NoteList> {
+  int count = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notes'),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: getListView(context),
+    );
+  }
+
+  ListView getListView(BuildContext context) {
+    TextStyle? textStyle = Theme.of(context).textTheme.bodyLarge;
+    return ListView.builder(
+        itemCount: count,
+        itemBuilder: (BuildContext context, int position) {
+          return Card(
+            color: Colors.white,
+            elevation: 2.0,
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.yellow,
+                child: Icon(Icons.add),
+              ),
+              title: Text(
+                'Dummy Title',
+                style: textStyle,
+              ),
+              subtitle: Text('Dummy Date'),
+              trailing: Icon(Icons.delete),
+              onTap: () {
+                debugPrint('ListTab');
+              },
+            ),
+          );
+        });
+  }
+}
